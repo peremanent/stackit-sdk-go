e9c01f04cc865e0b81494f3a91c700ccf2443329
Pere Manent <pere.manent@mail.schwarz>
2025-04-16 13:28:13 +0200

    Add waiters in git service for Create and Delete git instances

---
+++ services/git/wait/wait.go
@@ -0,0 +1,61 @@
+package wait
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/stackitcloud/stackit-sdk-go/core/wait"
+	"github.com/stackitcloud/stackit-sdk-go/services/git"
+)
+
+const (
+	CreateSuccess = "Ready"
+	Creating      = "Creating"
+	CreateFail    = "Error"
+)
+
+// APIClientInterface Interfaces needed for tests
+type APIClientInterface interface {
+	GetGitExecute(ctx context.Context, projectId string, instanceId string) (*git.Instance, error)
+}
+
+func CreateGitInstanceWaitHandler(ctx context.Context, a APIClientInterface, projectId, instanceId string) *wait.AsyncActionHandler[git.Instance] {
+	handler := wait.New(func() (waitFinished bool, response *git.Instance, err error) {
+		//crete the handler of the async action
+		instance, err := a.GetGitExecute(ctx, projectId, instanceId)
+		if err != nil {
+			return false, nil, err
+		}
+		//Check if the instance.Id is empty
+		if *instance.Id == "" || *instance.State == "" {
+			return false, nil, fmt.Errorf("could not get Instance id or State from response for project %s and instanceId %s", projectId, instanceId)
+		}
+		if *instance.Id == instanceId && *instance.State == CreateSuccess {
+			return true, instance, nil
+		}
+		if *instance.Id == instanceId && *instance.State == CreateFail {
+			return true, instance, fmt.Errorf("create failed for Instance with id %s", instanceId)
+		}
+		return false, nil, nil
+	})
+	handler.SetTimeout(10 * time.Minute)
+	return handler
+}
+
+func DeleteGitInstanceWaitHandler(ctx context.Context, a APIClientInterface, projectId, instanceId string) *wait.AsyncActionHandler[git.Instance] {
+	handler := wait.New(func() (waitFinished bool, response *git.Instance, err error) {
+		//create the handler of the async action
+		instance, err := a.GetGitExecute(ctx, projectId, instanceId)
+		if err != nil {
+			return true, nil, err
+		}
+		if instance != nil {
+			return true, instance, nil
+		}
+		//If there is no instance, it means it was deleted successfully
+		return true, nil, nil
+	})
+	handler.SetTimeout(10 * time.Minute)
+	return handler
+}
